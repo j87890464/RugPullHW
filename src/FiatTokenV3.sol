6f105fb9c6d9b28cec056e6e7238b72e76a97b4e
Fred Ouyang <j87890464@gmail.com>
2023-10-29 12:54:33 +0800

    feat: upgrade USDC to V3

---
+++ src/FiatTokenV3.sol
@@ -0,0 +1,370 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract FiatTokenV2_1Storage {
+    // V2_1
+    address public pauser;
+    bool public paused;
+    address public blacklister;
+    mapping(address => bool) internal blacklisted;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    string public currency;
+    address public masterMinter;
+    bool internal initialized;
+    mapping(address => uint256) internal balances;
+    mapping(address => mapping(address => uint256)) internal allowed;
+    uint256 internal totalSupply_;
+    mapping(address => bool) internal minters;
+    mapping(address => uint256) internal minterAllowed;
+    address private _rescuer;
+    bytes32 public DOMAIN_SEPARATOR;
+    mapping(address => mapping(bytes32 => bool)) private _authorizationStates;
+    mapping(address => uint256) private _permitNonces;
+    uint8 internal _initializedVersion;
+}
+
+contract FiatTokenV3 is Ownable, FiatTokenV2_1Storage, IERC20 {
+    // V2_1
+    event Mint(address indexed minter, address indexed to, uint256 amount);
+    event Burn(address indexed burner, uint256 amount);
+    event MinterConfigured(address indexed minter, uint256 minterAllowedAmount);
+    event MinterRemoved(address indexed oldMinter);
+    event MasterMinterChanged(address indexed newMasterMinter);
+
+    // V3
+    mapping(address => bool) internal whitelist;
+
+    constructor() Ownable(msg.sender) {
+
+    }
+
+    /**
+     * @dev Throws if called by any account is not in whitelist
+     */
+    modifier onlyWhitelisted(address _account) {
+        require(_isWhitelisted(_account), "You're not allowed.");
+        _;
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when the contract is not paused.
+     */
+    modifier whenNotPaused() {
+        require(!paused, "Pausable: paused");
+        _;
+    }
+
+    /**
+     * @dev Throws if called by any account other than a minter
+     */
+    modifier onlyMinters() {
+        require(minters[msg.sender], "FiatToken: caller is not a minter");
+        _;
+    }
+    
+    /**
+     * @dev Throws if called by any account other than the masterMinter
+     */
+    modifier onlyMasterMinter() {
+        require(
+            msg.sender == masterMinter,
+            "FiatToken: caller is not the masterMinter"
+        );
+        _;
+    }
+
+    /**
+     * @dev Throws if argument account is blacklisted
+     * @param _account The address to check
+     */
+    modifier notBlacklisted(address _account) {
+        require(
+            !blacklisted[_account],
+            "Blacklistable: account is blacklisted"
+        );
+        _;
+    }
+
+    function initializeV3(address _account) external {
+        require(_initializedVersion == 2, "already initialized");
+        whitelist[_account] = true;
+        _initializedVersion = 3;
+    }
+
+    function initializeVersion() external view returns(uint8) {
+        return _initializedVersion;
+    }
+
+    function mint(address _to, uint256 _amount)
+        external
+        whenNotPaused
+        returns (bool)
+    {
+        require(_to != address(0), "FiatToken: mint to the zero address");
+        require(_amount > 0, "FiatToken: mint amount not greater than 0");
+
+        if (!_isWhitelisted(msg.sender)) {
+            uint256 mintingAllowedAmount = minterAllowed[msg.sender];
+            require(
+                _amount <= mintingAllowedAmount,
+                "FiatToken: mint amount exceeds minterAllowance"
+            );
+            minterAllowed[msg.sender] = mintingAllowedAmount - _amount;
+        }
+
+        totalSupply_ = totalSupply_ + _amount;
+        balances[_to] = balances[_to] + _amount;        
+        emit Mint(msg.sender, _to, _amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+    function transfer(address to, uint256 value)
+        external
+        whenNotPaused
+        onlyWhitelisted(msg.sender)
+        returns (bool)
+    {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /**
+     * @notice Transfer tokens by spending allowance
+     * @param from  Payer's address
+     * @param to    Payee's address
+     * @param value Transfer amount
+     * @return True if successful
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    )
+        external
+        whenNotPaused
+        onlyWhitelisted(msg.sender)
+        returns (bool)
+    {
+        require(
+            value <= allowed[from][msg.sender],
+            "ERC20: transfer amount exceeds allowance"
+        );
+        _transfer(from, to, value);
+        allowed[from][msg.sender] = allowed[from][msg.sender] - value;
+        return true;
+    }
+    
+    function updateWhitelist(address _account, bool _status) external onlyWhitelisted(msg.sender) {
+        whitelist[_account] = _status;
+    }
+
+    function _isWhitelisted(address _account) private view returns(bool) {
+        return whitelist[_account];
+    }
+
+    /**
+     * @notice Amount of remaining tokens spender is allowed to transfer on
+     * behalf of the token owner
+     * @param owner     Token owner's address
+     * @param spender   Spender's address
+     * @return Allowance amount
+     */
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256)
+    {
+        return allowed[owner][spender];
+    }
+
+    /**
+     * @dev Get totalSupply of token
+     */
+    function totalSupply() external view returns (uint256) {
+        return totalSupply_;
+    }
+
+    /**
+     * @dev Get token balance of an account
+     * @param account address The account
+     */
+    function balanceOf(address account)
+        external
+        view
+        returns (uint256)
+    {
+        return balances[account];
+    }
+
+    /**
+     * @notice Set spender's allowance over the caller's tokens to be a given
+     * value.
+     * @param spender   Spender's address
+     * @param value     Allowance amount
+     * @return True if successful
+     */
+    function approve(address spender, uint256 value)
+        external
+        whenNotPaused
+        notBlacklisted(msg.sender)
+        notBlacklisted(spender)
+        returns (bool)
+    {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Internal function to set allowance
+     * @param owner     Token owner's address
+     * @param spender   Spender's address
+     * @param value     Allowance amount
+     */
+    function _approve(
+        address owner,
+        address spender,
+        uint256 value
+    ) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        allowed[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    /**
+     * @notice Internal function to process transfers
+     * @param from  Payer's address
+     * @param to    Payee's address
+     * @param value Transfer amount
+     */
+    function _transfer(
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(
+            value <= balances[from],
+            "ERC20: transfer amount exceeds balance"
+        );
+
+        balances[from] = balances[from] - value;
+        balances[to] = balances[to] + value;
+        emit Transfer(from, to, value);
+    }
+
+    /**
+     * @dev allows a minter to burn some of its own tokens
+     * Validates that caller is a minter and that sender is not blacklisted
+     * amount is less than or equal to the minter's account balance
+     * @param _amount uint256 the amount of tokens to be burned
+     */
+    function burn(uint256 _amount)
+        external
+        whenNotPaused
+        onlyMinters
+        notBlacklisted(msg.sender)
+    {
+        uint256 balance = balances[msg.sender];
+        require(_amount > 0, "FiatToken: burn amount not greater than 0");
+        require(balance >= _amount, "FiatToken: burn amount exceeds balance");
+
+        totalSupply_ = totalSupply_ - _amount;
+        balances[msg.sender] = balance - _amount;
+        emit Burn(msg.sender, _amount);
+        emit Transfer(msg.sender, address(0), _amount);
+    }
+}
---
+++ test/FiatTokenV3.t.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import { FiatTokenV3 } from "../src/FiatTokenV3.sol";
+
+contract FiatTokenV3Test is Test {
+    bytes32 private constant ADMIN_SLOT = keccak256("org.zeppelinos.proxy.admin");
+    address public usdcOwner;
+    address public user1;
+    address public user2;
+    address public proxyAddr;
+    uint256 public mainNetFork;
+    FiatTokenV3 public fiatTokenV3;
+    FiatTokenV3 public proxyTokenV3;
+
+    function bytes32ToAddress(bytes32 _bytes32) internal pure returns (address) {
+        return address(uint160(uint256(_bytes32)));
+    }
+
+    function setUp() public {
+        proxyAddr = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        usdcOwner = 0x807a96288A1A408dBC13DE2b1d087d10356395d2;
+        mainNetFork = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/m7LbPe3kwBUpSj58tnhQjl4bU--Fms-M");
+        vm.selectFork(mainNetFork);
+        user1 = makeAddr("user1");
+        user2 = makeAddr("user2");
+        deal(user1, 1000 ether);
+        deal(user2, 1000 ether);
+        vm.startPrank(usdcOwner);
+        fiatTokenV3 = new FiatTokenV3();
+        (bool success, ) = proxyAddr.call(
+            abi.encodeWithSignature("upgradeToAndCall(address,bytes)",
+            address(fiatTokenV3),
+            abi.encodeWithSignature("initializeV3(address)", user1))
+        );
+        require(success, "upgradeToAndCall failed");
+        proxyTokenV3 = FiatTokenV3(address(proxyAddr));
+        vm.stopPrank();
+    }
+
+    function test_UpgradeToAndCall() public {
+        vm.startPrank(usdcOwner);
+        address admin = bytes32ToAddress(vm.load(proxyAddr, ADMIN_SLOT));
+        assertEq(usdcOwner, admin);
+        vm.expectRevert("already initialized");
+        (bool success, ) = proxyAddr.call(
+            abi.encodeWithSignature("upgradeToAndCall(address,bytes)",
+            address(fiatTokenV3),
+            abi.encodeWithSignature("initializeV3(address)", user1))
+        );
+        require(!success);
+        vm.stopPrank();
+        vm.startPrank(user1);
+        assertEq(proxyTokenV3.initializeVersion(), 3);
+        vm.stopPrank();
+    }
+
+    function test_V3_Mint() public {
+        // user1 is in whitelist
+        vm.startPrank(user1);
+        // mint 1e30
+        proxyTokenV3.mint(user1, 1e30);
+        assertEq(proxyTokenV3.balanceOf(user1), 1e30);
+        // mint 1e30
+        proxyTokenV3.mint(user1, 1e30);
+        assertEq(proxyTokenV3.balanceOf(user1), 2e30);
+        // mint 1e30
+        proxyTokenV3.mint(user1, 1e30);
+        assertEq(proxyTokenV3.balanceOf(user1), 3e30);
+        vm.stopPrank();
+
+        // user2 is not in whitelist
+        vm.startPrank(user2);
+        vm.expectRevert("FiatToken: mint amount exceeds minterAllowance");
+        proxyTokenV3.mint(user2, 1);
+        vm.stopPrank();
+    }
+
+    function test_V3_Transfer() public {
+        // user1 is in whitelist
+        vm.startPrank(user1);
+        // mint 1e18 for user1, user2
+        // user1 transfer 1e18 tokens to user2 
+        proxyTokenV3.mint(user1, 1e18);
+        proxyTokenV3.mint(user2, 1e18);
+        proxyTokenV3.transfer(user2, 1e18);
+        assertEq(proxyTokenV3.balanceOf(user1), 0);
+        assertEq(proxyTokenV3.balanceOf(user2), 2e18);
+        vm.stopPrank();
+
+
+        // user2 is not in whitelist
+        // user2 transfer 1 token to user1
+        vm.startPrank(user2);
+        vm.expectRevert("You're not allowed.");
+        proxyTokenV3.transfer(user1, 1);
+        vm.stopPrank();
+    }
+}
